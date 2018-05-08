c3f929c3fa2fed7627e7871111f3d7c1a322a978
David Zeit <dzeit216@gmail.com>
2018-05-08 21:39:47 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,5 +5,5 @@ ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   esp_phrase = arr.map {|phrase| phrase.split("")} 
   arr.map {|phrase| phrase.split("")} 
-  esp_phrase.sort_by! {|phrase| phrase[0]}
+  esp_phrase.sort_by! {|phrase| phrase.index[0]}
 end
