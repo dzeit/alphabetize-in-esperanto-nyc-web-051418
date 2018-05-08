96d54db7fd06e4880555db4995a768dc36a7923d
David Zeit <dzeit216@gmail.com>
2018-05-08 21:54:33 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,3 +6,6 @@ def alphabetize(arr)
   esp_phrase = arr.map {|phrase| phrase.split("")} 
   esp_phrase.sort_by! {|phrase| phrase.map {|letter| ALPHABET.index(letter)}}.map {|array| array.join}
 end
+
+# Shorter method
+#arr.sort_by {|phrase| phrase.split("").map{|letter| ALPHABET.index(letter)}}
