c55ba28ae180dbd12f8435b36fda766fc13d188e
David Zeit <dzeit216@gmail.com>
2018-05-08 21:56:54 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  esp_phrase = arr.map {|phrase| phrase.split("")} 
+  esp_phrase.sort_by! {|phrase| phrase.map {|letter| ALPHABET.index(letter)}}.map {|array| array.join}
 end
+
+# Shorter method
+#arr.sort_by {|phrase| phrase.split("").map{|letter| ALPHABET.index(letter)}} 
